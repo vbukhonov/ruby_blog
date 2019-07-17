998b1bc61c0121db767e30e2d074be6241f303e2
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 21:55:28 +0300

    Connect User and Blog.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 5 }
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :email
   validates_presence_of :password, on: :create
+  has_many :blogs
 end
