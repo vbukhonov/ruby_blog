df8538734bd9ea1d6a1b07f729d9d307f7847d4e
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 20:51:57 +0300

    Add SessionsController controller.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'sign_up', to: 'users#new', as: 'sign_up'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
+  resources :sessions
   resources :blogs
 
   root 'welcome#index'
