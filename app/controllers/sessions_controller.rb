5eb080716aca7049b6abd210c0daa936db375e7b
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 21:17:28 +0300

    Implement "create" method and add "new" method for SessionsController.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
 
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged In!'
+    else
+      flash.now.alert = 'Email or password is incorrect'
+      render 'new'
+    end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged Out!'
+  end
+
 end
