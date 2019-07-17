f232046679645f789dcfc6a00aa53d51a6d1bb9f
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 14:50:44 +0300

    Add "destroy" action for the Blog app.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -36,6 +36,13 @@ class BlogsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    redirect_to blogs_path
+  end
+
   private
 
   def blog_params
