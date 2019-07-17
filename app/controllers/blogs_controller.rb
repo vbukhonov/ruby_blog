a4880cb0bcd8372f1c98a4ee9e7f518daeee6ddb
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 22:16:51 +0300

    Add the current_user usage for the new, edit, create, update and destroy methods.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
 
-  before_action :authorize, only: %i[edit update destroy]
+  before_action :authorize, only: %i[new edit create update destroy]
 
   def index
     @blogs = Blog.all
@@ -11,16 +11,15 @@ class BlogsController < ApplicationController
   end
 
   def new
-    @blog = Blog.new
+    @blog = @current_user.blogs.new
   end
 
   def edit
-    @blog = Blog.find(params[:id])
+    @blog = @current_user.blogs.find(params[:id])
   end
 
   def create
-    @blog = Blog.new(blog_params)
-
+    @blog = @current_user.blogs.create(blog_params)
     if @blog.save
       redirect_to @blog
     else
@@ -29,7 +28,7 @@ class BlogsController < ApplicationController
   end
 
   def update
-    @blog = Blog.find(params[:id])
+    @blog = @current_user.blogs.find(params[:id])
 
     if @blog.update(blog_params)
       redirect_to @blog
@@ -39,7 +38,7 @@ class BlogsController < ApplicationController
   end
 
   def destroy
-    @blog = Blog.find(params[:id])
+    @blog = @current_user.blogs.find(params[:id])
     @blog.destroy
 
     redirect_to blogs_path
