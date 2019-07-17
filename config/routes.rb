35f8d94f6e5373ee3f1d9d0a1e8f169e2e88b89c
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 20:49:48 +0300

    Add User model and UsersController controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see:
   # http://guides.rubyonrails.org/routing.html
+
   get 'welcome/index'
+
+  get 'sign_up', to: 'users#new', as: 'sign_up'
+
+  resources :users
   resources :blogs
+
   root 'welcome#index'
 end
