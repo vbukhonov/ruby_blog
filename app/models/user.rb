f2e0b131593ab90f81aa1f108a16b650d3649e51
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-07-17 21:58:19 +0300

    Add dependent: :destroy setting for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :email
   validates_presence_of :password, on: :create
-  has_many :blogs
+  has_many :blogs, dependent: :destroy
 end
